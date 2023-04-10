4494e811572b46e7b4173d7057533f1ec9fdd2e1
M Isa Al Anshori <isabungsu@gmail.com>
2023-04-10 20:00:00 +0700

    rapihkan main DatabaseConnection

--- src/main/java/pw/misa/kk6/Database/DatabaseConnection.java
+++ src/main/java/pw/misa/kk6/Database/DatabaseConnection.java
@@ -7,7 +7,7 @@ abstract public class DatabaseConnection implements DocumentAccesable, Collectio
             DatabaseConnection db = new SimpleDB();
             
             // Demo DocumentAccesable
-            System.out.println("DEMO DOCUMENTACCESSABLE");
+            System.out.println("==DEMO DOCUMENTACCESSABLE==");
             // Membuat dokumen tanpa password
             System.out.println("Membuat dokumen tanpa password");
             String docNoPass = db.createDocument("Hello World", "Hello World is a common test program usually used by programmers to test that their software is working");
@@ -74,21 +74,23 @@ abstract public class DatabaseConnection implements DocumentAccesable, Collectio
             System.out.println("docWithPass Exists?: " + db.checkDocument(docWithPass));
             
             // ==============Demo CollectionsAccessable=================
-            System.out.println("\n\nDEMO COLLECTIONSACCESSSABLE");
+            System.out.println("\n\n==DEMO COLLECTIONSACCESSSABLE==");
             
             // buat ulang docWithPass
             docWithPass = db.createDocument("A New Document", "This is a new document with a password", "goodPassword");
             
-            // Membuat Collection tanpa password
-            System.out.println("Membuat Collection tanpa password baru dengan isi docNoPass dan docWithPass");
             List<String> listDokumen = new ArrayList<String>();
             List<String> isiCollection;
             listDokumen.add(docNoPass);
             listDokumen.add(docWithPass);
+            
+            // Membuat Collection tanpa password
+            System.out.println("Membuat Collection tanpa password baru dengan isi docNoPass dan docWithPass");
             String colNoPass = db.createCollection("2 documents", listDokumen);
+            System.out.println("colNoPass ID: " + colNoPass);
             
             // Cek Collections dan Isinya
-            System.out.println("Cek Collections dan Isinya");
+            System.out.println("\nCek Collections dan Isinya");
             System.out.println("colNoPass exists? " + db.checkCollection(colNoPass));
             System.out.println("random collection exists? " + db.checkCollection("124214312"));
             System.out.println("colNoPass exists with password? " + db.checkCollection(colNoPass, "a"));
@@ -100,7 +102,7 @@ abstract public class DatabaseConnection implements DocumentAccesable, Collectio
             }
             
             // Mencoba Mengupdate Collections Tanpa Password
-            System.out.println("Mencoba Mengupdate Collections Tanpa Password");
+            System.out.println("\nMencoba Mengupdate Collections Tanpa Password");
             System.out.println("colNoPass update title: " + db.updateCollectionTitle(colNoPass, null, "3 documents in here"));
             listDokumen.add(db.createDocument("Sebuah Dokumen", "Ini adalah sebuah dokumen", "passwordBagus"));
             System.out.println("colNoPass update contents: " + db.updateCollectionContents(colNoPass, null, listDokumen));
@@ -111,7 +113,7 @@ abstract public class DatabaseConnection implements DocumentAccesable, Collectio
             }
             
             // Mencoba Menghapus Collections Tanpa Password
-            System.out.println("Mencoba Menghapus Collections Tanpa Password");
+            System.out.println("\nMencoba Menghapus Collections Tanpa Password");
             System.out.println("colNoPass delete: " + db.deleteCollection(colNoPass, ""));
             System.out.println("colNoPass contents: ");
             isiCollection = db.getCollectionContents(colNoPass);
@@ -119,12 +121,15 @@ abstract public class DatabaseConnection implements DocumentAccesable, Collectio
                 System.out.printf("%d. %s (%s)\n", i+1, isiCollection.get(i), db.getDocumentTitle(isiCollection.get(i)));
             }
             
+            System.out.println("============================================================");
+            
             // Membuat Collections dengan password
             System.out.println("\nMembuat Collections dengan Password");
             String colWithPass = db.createCollection("3 documents", listDokumen, "goodPassword");
+            System.out.println("colWithPass ID: " + colWithPass);
             
             // Cek Collections dan Isinya
-            System.out.println("Cek Collections dan Isinya");
+            System.out.println("\nCek Collections dan Isinya");
             System.out.println("colWithPass exists? " + db.checkCollection(colWithPass));
             System.out.println("colWithPass exists with password? " + db.checkCollection(colWithPass, "goodPassword"));
             System.out.println("colWithPass exists with wrong password? " + db.checkCollection(colWithPass, "a"));
@@ -137,7 +142,7 @@ abstract public class DatabaseConnection implements DocumentAccesable, Collectio
             }
             
             // Mencoba Mengupdate Collections
-            System.out.println("Mencoba Mengupdate Collections");
+            System.out.println("\nMencoba Mengupdate Collections");
             System.out.println("colWithPass update title null password: " + db.updateCollectionTitle(colWithPass, null, "4 documents in here"));
             System.out.println("colWithPass update title: " + db.updateCollectionTitle(colWithPass, "goodPassword", "4 documents in here"));
             listDokumen.add(db.createDocument("Sebuah Dokumen e2", "Ini adalah sebuah dokumen", "passwordBagus"));
@@ -147,6 +152,13 @@ abstract public class DatabaseConnection implements DocumentAccesable, Collectio
             for (int i = 0; i < isiCollection.size(); i++) {
                 System.out.printf("%d. %s (%s)\n", i+1, isiCollection.get(i), db.getDocumentTitle(isiCollection.get(i)));
             }
+            
+            // Mencoba Menghapus Collection
+            System.out.println("\nMencoba Menghapus Collections");
+            System.out.println("colWithPass delete wrong password: " + db.deleteCollection(colWithPass, "wrongpass"));
+            System.out.println("colWithPass exists? " + db.checkCollection(colWithPass));
+            System.out.println("colWithPass delete: " + db.deleteCollection(colWithPass, "goodPassword"));
+            System.out.println("colWithPass exists? " + db.checkCollection(colWithPass));
         }
         
         protected static String genID() {
